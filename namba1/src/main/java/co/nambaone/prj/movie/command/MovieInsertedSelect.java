545c7e713667d4e38de425c427fa8d35a218df1a
lwedubloomd <kkah0210@naver.com>
2022-12-26 12:26:10 +0900

    test

--- namba1/src/main/java/co/nambaone/prj/movie/command/MovieInsertedSelect.java
+++ namba1/src/main/java/co/nambaone/prj/movie/command/MovieInsertedSelect.java
@@ -20,6 +20,6 @@ public class MovieInsertedSelect implements Command {
 		MovieVO vo = new MovieVO();
 		List<MovieVO> list = service.insertedSelect(vo);
 		return null;
-	}안녕하심니까.....
+	}안녕하심니까.....ㅇㅇㅇdddd
 
 }
